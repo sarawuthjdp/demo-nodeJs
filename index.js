17b7cbe65786cfbf1fa4ba1bcc631305611f1543
sarawuthjdp <sarawuth.jindapan@gmail.com>
2021-11-04 22:56:31 +0700

    Promise

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-const url1='zen.dev/file1.json'
-const url2='zen.dev/file2.json'
-const url3='zen.dev/file3.json'
+const url1 = "zen.dev/file1.json";
+const url2 = "zen.dev/file2.json";
+const url3 = "zen.dev/file3.json";
 
-function downloading(url,callback){
-    console.log(`กำลังโหลดไฟล์ ${url}`)
-    setTimeout(()=>{
-        callback(url);
-    },3000)
+const connect = true;
+function downloading(url) {
+  return new Promise((resovle, reject) => {
+    console.log(`กำลังดาวน์โหลด...`);
+    setTimeout(() => {
+      if (connect) {
+        resovle(`โหลด ${url} เรียบร้อย`);
+      } else {
+        reject(`เกิดข้อผิดพลาด`);
+      }
+    }, 3000);
+  });
 }
 
-downloading(url1,(result)=>{
-    console.log(`ดาวน์โหลด ${result} เรียบร้อย`)
-    downloading(url2,(result)=>{
-        console.log(`ดาวน์โหลด ${result} เรียบร้อย`)
-        downloading(url3,(result)=>{
-            console.log(`ดาวน์โหลด ${result} เรียบร้อย`)
-        })
-    })
-})
+//PromiseHell
+// downloading(url1).then((result)=>{
+//     console.log(result)
+//     downloading(url2).then((result)=>{
+//         console.log(result)
+//         downloading(url3).then((result)=>{
+//             console.log(result)
+//         })
+//     })
+// })
+
+downloading(url1)
+  .then((result) => {
+    console.log(result);
+    return downloading(url2);
+  })
+  .then((result) => {
+    console.log(result);
+    return downloading(url3);
+  })
+  .then((result) => {
+    console.log(result);
+  });
