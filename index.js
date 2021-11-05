4c74a0da07265c4b7c0d4b927ff9b6f34151e100
sarawuthjdp <sarawuth.jindapan@gmail.com>
2021-11-06 00:31:57 +0700

    routing

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const http = require('http')
 
 http.createServer((req,res)=>{
-  const myHtml=`
-    <h1>Hello World!</h1>
-    <p style="color:green">Test demo node.js</p>
-  `
-  res.write(myHtml)
-  res.end()
-}).listen(8080,'localhost',()=>{
-  console.log('start server in port 8080')
+  const pathName = req.url;
+  if(pathName==='/'||pathName==='/home'){
+    res.end(`<h1>Hello homepage!</h1>`)
+  }else if(pathName === '/product'){
+    res.end(`<h1>Hello product!</h1>`)
+  }else{
+    res.writeHead(404)
+    res.end(`<h1>not found!</h1>`)
+  }
+}).listen(3000,'localhost',()=>{
+  console.log('start server in port 3000')
 })
