ca748da0a9219e981464f410c6d0dcf418e26585
sarawuthjdp <sarawuth.jindapan@gmail.com>
2021-11-07 19:11:19 +0700

    using express.js

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-const http = require('http')
-const fs = require('fs')
-const url = require('url')
-http.createServer((req,res)=>{
-  const {pathname,query} = url.parse(req.url,true);
-  console.log(pathname)
-  if(pathname==='/'||pathname==='/home'){
-    res.end(fs.readFileSync(`${__dirname}/templates/index.html`,'utf-8'))
-  }else if(pathname === '/product'){
-    if(query.id === '1'){
-      res.end(fs.readFileSync(`${__dirname}/templates/product1.html`,'utf-8'))
-    }else if(query.id === '2'){
-      res.end(fs.readFileSync(`${__dirname}/templates/product2.html`,'utf-8'))
-    }else if(query.id === '3'){
-      res.end(fs.readFileSync(`${__dirname}/templates/product3.html`,'utf-8'))
-    }else{
-      res.writeHead(404)
-      res.end(`<h1>not found!</h1>`)
-    }
-  }else{
-    res.writeHead(404)
-    res.end(`<h1>not found!</h1>`)
-  }
-}).listen(3000,'localhost',()=>{
-  console.log('start server in port 3000')
+const express = require('express')
+const app = express()
+
+app.use((req,res)=>{
+  res.send('<h1>Hello Express.js</h1>')
+}).listen(8080,()=>{
+  console.log('Start server at port 8080.')
 })
