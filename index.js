18dffaa26ac8f58fa3a76abe34edb3b44a94c9c7
sarawuthjdp <sarawuth.jindapan@gmail.com>
2021-11-07 19:22:03 +0700

    get post method

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const app = express()
 
-app.use((req,res)=>{
+app.get(`/`,(req,res)=>{
   res.send('<h1>Hello Express.js</h1>')
-}).listen(8080,()=>{
+})
+
+app.get(`/product`,(req,res)=>{
+  res.send('<h1>Hello product</h1>')
+})
+
+app.listen(8080,()=>{
   console.log('Start server at port 8080.')
 })
