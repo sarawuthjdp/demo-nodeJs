c9e84b7bd65234618a9cccec401ef5e03143b80b
sarawuthjdp <sarawuth.jindapan@gmail.com>
2021-11-07 20:05:25 +0700

    router parameter

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const path = require('path')
+const path = require("path");
 
 router.get("/", (req, res) => {
   res.status(200);
@@ -8,10 +8,20 @@ router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../templates/index.html"));
 });
 
-router.get("/product", (req, res) => {
+router.get("/product/:id", (req, res) => {
+  const myparam = req.params.id;
   res.status(200);
   res.type("text/html");
-  res.sendFile(path.join(__dirname, "../templates/product1.html"));
+  if (myparam === "1") {
+    res.sendFile(path.join(__dirname, "../templates/product1.html"));
+  } else if (myparam === "2") {
+    res.sendFile(path.join(__dirname, "../templates/product2.html"));
+  } else if (myparam === "3") {
+    res.sendFile(path.join(__dirname, "../templates/product3.html"));
+  } else {
+    res.status(404);
+    res.send("<h1>Not found</h1>");
+  }
 });
 
-module.exports = router
+module.exports = router;
