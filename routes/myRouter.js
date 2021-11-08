72298f61fbccba9646157a08edf00effa69aa0b8
sarawuthjdp <sarawuth.jindapan@gmail.com>
2021-11-08 15:08:30 +0700

    static files

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -2,25 +2,4 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-router.get("/", (req, res) => {
-  res.status(200);
-  res.type("text/html");
-  res.sendFile(path.join(__dirname, "../templates/index.html"));
-});
-
-router.get("/product/:id", (req, res) => {
-  const myparam = req.params.id;
-  res.status(200);
-  res.type("text/html");
-  if (myparam === "1") {
-    res.sendFile(path.join(__dirname, "../templates/product1.html"));
-  } else if (myparam === "2") {
-    res.sendFile(path.join(__dirname, "../templates/product2.html"));
-  } else if (myparam === "3") {
-    res.sendFile(path.join(__dirname, "../templates/product3.html"));
-  } else {
-      res.redirect('/')
-  }
-});
-
 module.exports = router;
